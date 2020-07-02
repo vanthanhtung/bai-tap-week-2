9d200541a7dcf72a021f9c73cd2471f43efd314f
vanthanhtung <vanthanhtung1562013@gmail.com>
2020-07-02 14:59:52 +0700

    all

---
+++ Nghich Js/nghichjs.js
@@ -0,0 +1,23 @@
+switch (new Date().getDay()) {
+    case 0:
+        day = "Chủ Nhật";
+        break;
+    case 1:
+        day = "Thứ hai";
+        break;
+    case 2:
+        day = "Thứ ba";
+        break;
+    case 3:
+        day = "Thứ tư";
+        break;
+    case 4:
+        day = "Thứ năm";
+        break;
+    case 5:
+        day = "Thứ sáu";
+        break;
+    case 6:
+        day = "Thứ bảy";
+        break;
+}
